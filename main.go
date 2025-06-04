9f0128afa10ca1f524cf4ba9d87ac14458956948
Askhat Gumirov <agumirovz@gmail.com>
2025-06-04 23:44:36 +0500

    test123

--- main.go
+++ main.go
@@ -3,21 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	d := NewDequeue(3, 3)
-
-	go squares(c)
-
-type OptionalInt struct {
-	Value int
-	IsSet bool
-}
-
-	fmt.Println("main stop")
-}
-
-func squares(c chan int) {
-	for i := 0; i <= 2; i++ {
-		num := <-c
-		fmt.Println(num * num)
-	}
+	fmt.Println("Hello world!")
 }
